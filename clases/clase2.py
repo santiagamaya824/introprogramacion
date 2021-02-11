45b8770483154b967a1f1fa7c8ec7a8c113d6ed8
santiagoamaya824 <santii824@hotmail.com>
2021-02-11 07:15:52 -0500

    clase 2

--- clases/clase2.py
+++ clases/clase2.py
@@ -7,4 +7,17 @@ print (pruebav)
 edad = 20
 estatura = 1.70
 peso = 70
+nombre = "santiago amaya mendez"
+print ("#"*15,"mayor edad", "#"*15)
+isMayorEdad = edad >= 18
+print (isMayorEdad)
+print ("#"*15,"bajo la estatura promedio","#"*15)
+isMayorEstatura = estatura < 1.70
+print (isMayorEstatura)
+print ("#"*15,"peso diferente 84", "#"*15)
+isPesoDiferente = peso != 84
+print( isPesoDiferente)
+
+
+
 
