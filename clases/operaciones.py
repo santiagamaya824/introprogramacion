6fe4f6b9f8f9735398e29fd0ea1142f97c58c0ad
santiagoamaya824 <santii824@hotmail.com>
2021-02-11 07:38:39 -0500

    operaciones

---
+++ clases/operaciones.py
@@ -0,0 +1,10 @@
+numeroA = 87
+numeroB = 83
+sumar = numeroA + numeroB
+print("el resultado de la suma es",sumar)
+restar= numeroA - numeroB
+print("el resultado de la resta es",restar)
+multiplicar = numeroA * numeroB
+print("el resultado de la multiplicacion es",multiplicar)
+dividir = numeroA / numeroB
+print("el resultado de la division es",dividir)
