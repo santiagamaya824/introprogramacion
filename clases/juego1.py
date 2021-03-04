d6d1f30a2f1d9fc0da87b2aa16042bff4a8a02ab
santiagoamaya824 <santii824@hotmail.com>
2021-03-04 07:35:58 -0500

    juego

--- clases/juego1.py
+++ clases/juego1.py
@@ -1,7 +1,7 @@
 #juego con while e if 
 #....constantes....#
 saludo = "bienbenido, juguemos"
-pregunta_numero = "en  este juego debes aceptar un numero q va del 1 al 10, lo puedes intentar las veces que quieras....exitos, infresa tu numero : "
+pregunta_numero = "en  este juego debes adivinar un numero q va del 1 al 10, lo puedes intentar las veces que quieras....exitos, ingresa tu numero : "
 pregunta_fallaste = "fallaste, igresa otro numero : "
 mensaje_ganaste = "felicidades ganaste"
 mensaje_perdiste = "peridste, vuelve a intentarlo"
@@ -17,7 +17,7 @@ while (numeroOculto != numeroingresado and vidas >0):
     numeroingresado = int(input(pregunta_fallaste))
     vidas -=1
 
-if (vidas >= 0):
+if (vidas >= 0 and numeroingresado == numeroOculto):
     print(mensaje_ganaste)
     print(vidas)
 else:
