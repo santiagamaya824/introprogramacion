b7cd9763b41caa5440d535d8f7d468738a241674
santiagoamaya824 <santii824@hotmail.com>
2021-04-07 13:42:21 -0500

    ejemplo clase

--- TALLERES/ejemploquiz.py
+++ TALLERES/ejemploquiz.py
@@ -1,16 +1,16 @@
 # ---- Preguntas
-preguntaNumero  =  '' 'Ingrese alguna de estas opciones
+preguntaNumero  =  """Ingrese alguna de estas opciones
     1.Hacer conversión de pesos a dólares o euros
     2.Agrege un valor a la lista de pesos
     3.Mostrar valor más alto, más bajo y promedio
     4.Eliminar elemento de la lista
     5. Salir
-'' '
-preguntaMoneda  =  '' '
+"""
+preguntaMoneda  =  """
     C- Mostrar original en pesos colombiano
     D- Mostrar en Dolares
     E- Mostrar en Euros
-'' '
+"""
 preguntarNumero  =  'Ingrese un valor en COP:'
 preguntaBorrarCoordenada  =  'Ingrese la posición que desea borrar:'
 # ---- Mensajes --- #
@@ -39,7 +39,7 @@ for  elemento  in  listaPesos :
 
 
 opcionEscogida  =  int ( input ( preguntaNumero ))
-while ( opcionEscogida  ! = 5 ):
+while ( opcionEscogida  != 5 ):
     # --------- Opcion1 --------- #
     if ( opcionEscogida  ==  1 ):
         opcionMoneda  =  entrada ( preguntaMoneda )
@@ -52,7 +52,7 @@ while ( opcionEscogida  ! = 5 ):
         elif ( opcionMoneda  ==  'E' ):
             imprimir ( mensajeEuros )
             imprimir ( listaEuros )
-        otra cosa :
+        else :
             imprimir ( mensajeErrorEntrada )
     # --------- Opcion2 --------- #
     elif ( opcionEscogida  ==  2 ):
---
+++ clases/funciones/ejemplos_clase1.py
@@ -0,0 +1,67 @@
+guardar  =  imprimir ( 'hola' )
+imprimir ( guardar )
+
+guardar  =  redondo ( 14.2534897 , 2 )
+imprimir ( guardar )
+
+def  linedesign ( cantidad  =  10 , simbolo  =  '#' ):
+    imprimir ( simbolo  * cantidad )
+    return None
+
+linedesign ( 30 , '#' )
+linedesign ( 10 , '*' )
+linedesign ( 100 , '♦' )
+
+
+# ---------- Muestre la lista ----------- #
+def  mostrarLista ( listaEntrada  = []):
+    for elemento  in  listaEntrada :
+        imprimir ( elemento )
+    return  None
+lista = [ 213 , 32 , 23123 , 321 , 321 , 233 , 1232 , 23 ]
+lista2  = [ 564654 , 645 , 64543 , 2565 , 547 , 57865 ]
+linedesign() ( 30 , '☺' )
+mostrarLista ( lista )
+linedesign ( 3 , '(ง • ̀_ • ́) ง' )
+mostrarLista ( lista2 )
+# ---------- Sumar dos números ----------- #
+def  sumar ( a  =  0 , b  =  0 ):
+    suma  =  a  +  b
+    return suma
+linedesign() ( 30 , '♣' )
+resultado =  sumar ()
+imprimir ( resultado )
+imprimir ( sumar ( 12 , 14 ))
+redonda ( 12.234897 , 2 )
+# ---------- Restar dos números ----------- #
+def  restar ( a  =  0 , b  =  0 ):
+    resta  =  a  -  b
+    return resta
+# ---------- Multiplicar dos números ----------- #
+def  multiplicar ( a  =  0 , b  =  0 ):
+    multiplica  =  a  *  b
+    return multiplica
+# ---------- dividir dos números ----------- #
+def  dividir ( a  =  0 , b  =  1 ):
+    dividi  =  a  /  b
+    return dividi
+# ---------- potenciar dos números ----------- #
+def  potenciar ( base  =  0 , exponente  =  1 ):
+    potencia  =  base  **  exponente
+    return potencia
+
+# ---------- funciones dependientes de otras ----------- #
+def  calcular ( operacion , numeroA , numeroB ):
+    print ( operacion ( numeroA , numeroB ))
+
+
+baseIngresada  =  int ( input ( 'Ingrese una base entera:' ))
+exponenteIngresado  =  int ( input ( 'ingrese un exponente entero:' ))
+
+imprimir ( restar ( 83 , 87 ))
+imprimir ( multiplicar ( 83 , 87 ))
+imprimir ( dividir ( 83 , 87 ))
+imprimir ( dividir ())
+imprimir ( potenciar ( baseIngresada , exponenteIngresado ))
+
+calcular ( sumar , 63 , 67 )
